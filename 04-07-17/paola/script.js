62a75fcfdadc657930ba3cbd6ce489d3be62585d
data4 <alejandro.carreno@data4.mx>
2017-07-04 14:23:37 -0500

    "se agrego slider"

---
+++ 04-07-17/paola/script.js
@@ -0,0 +1,78 @@
+jQuery(document).ready(function () {
+  
+<script>
+  $(function () {
+    // 1. Initialize fotorama manually.
+    var $fotoramaDiv = $('#fotorama').fotorama();
+
+    // 2. Get the API object.
+    var fotorama = $fotoramaDiv.data('fotorama');
+
+    // 3. Inspect it in console.
+    console.log(fotorama);
+  });
+
+  fotorama.resize({
+  width: 500,
+  height: 333
+});
+</script
+
+    jQuery("#nanogallery2").nanogallery2(
+        {
+            items:                      [
+                                            {
+                                                src: 'berlin1t.jpg',		  // image url
+                                                srct: 'berlin1t.jpg',	  // thumbnail url
+                                                title: 'Yoda',      // element title
+                                                customData: { favorite: false }    // custom data -> favorite status
+                                            },
+                                            { src: 'berlin2t.jpg', srct: 'berlin2t.jpg', title: 'Rancor', customData: { favorite: true} },
+                                            { src: 'berlin3t.jpg', srct: 'berlin3t.jpg', title: 'Ortolanos', customData: { favorite: false} }
+                                        ],
+            thumbnailWidth:             'auto',
+            thumbnailHeight:            200,
+            locationHash:               false,
+            thumbnailHoverEffect2 :     '.ngy2info_opacity_0_1|.ngy2info_scale_5_1',
+            thumbnailBuildInit2:        '.ngy2info_translateY_-50%|.ngy2info_translateX_-50%',
+            thumbnailToolbarImage:      { topLeft: 'custom1' },
+            fnThumbnailInit:            myTnInit,
+            fnThumbnailToolCustAction:  myTnTool,
+            icons:                      { thumbnailCustomTool1 : '<i class="nGY2Icon icon-star"></i>'}
+
+        });
+
+});
+
+// click/touch on custom tool/button
+function myTnTool( action, item ) {
+    console.dir(item);
+    
+    switch( action ) {
+        case 'info':
+        alert('Photo title: '+item.title);
+        
+        break;
+        case 'custom1':
+        // switch favorite status
+        item.customData.favorite=!item.customData.favorite;
+        TnSetFavorite(item);
+        break;
+    }
+}
+
+// Add custom elements after one thumbnail is build
+function myTnInit( $e, item, GOMidx ) {
+    var st='position:absolute;top:50%;left:50%;padding:10px;'
+    $e.find('.nGY2GThumbnailSub').append('<button style="'+st+'" type="button" class="ngy2info" data-ngy2action="info">photo info</button>');
+    TnSetFavorite( item);
+}
+
+// Set the favorite status
+function TnSetFavorite(item) {
+    var c='#880';
+    if( item.customData.favorite ) {
+        c='#ff0';
+    }
+    item.$elt.find('[data-ngy2action="custom1"]').css('color',c);
+}
