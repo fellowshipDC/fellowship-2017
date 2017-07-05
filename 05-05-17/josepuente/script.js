0d591bafa01ef7456bdc7541e9599adf7ae25f44
Jose Luis <puenterodriguezjose@gmail.com>
2017-07-05 11:24:37 -0500

    Agrega galeria con imagenes y hr

---
+++ 05-05-17/josepuente/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    console.log('ready!')
+    $("[data-fancybox]").fancybox({
+        // Options will go here
+        loop: true, //loopea la galería
+    });
+});
