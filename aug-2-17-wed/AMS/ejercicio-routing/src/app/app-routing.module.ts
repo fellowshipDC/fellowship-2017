f106345b78cf15655688b6d75cd591fd6f7103b4
unknown <mirandas.angelica@gmail.com>
2017-08-02 11:59:32 -0500

    to login and back

---
+++ aug-2-17-wed/AMS/ejercicio-routing/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [],
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    children: [],
+    component: LoginComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
