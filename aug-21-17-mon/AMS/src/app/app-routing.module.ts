ba5ebaaa452790965644c216137a6636b1756079
unknown <mirandas.angelica@gmail.com>
2017-08-22 11:13:44 -0500

    ejercicio de clase sin abrir imágenes en el componente

---
+++ aug-21-17-mon/AMS/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ImagenComponent } from './imagen/imagen.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [],
+    component: HomeComponent
+  },
+  {
+    path: 'imagen',
+    children: [],
+    component: ImagenComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
