5c9a8a74dd27e6d5f33b209617f99c4441608663
Paola <Paola@MacBook-Pro-de-Paola.local>
2017-08-23 14:44:49 -0500

    add: ejercicio de hoy

---
+++ aug-23-17-wed/paola/src/app/book/book.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.styl'],
+  /*template:
+
+  `
+  <div>
+    Showing product details for product: {{title}}
+  </div>
+    `,*/
+})
+export class BookComponent implements OnInit {
+
+  private title;
+  private sub: any;
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.title = params ['title'];
+    });
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+}
