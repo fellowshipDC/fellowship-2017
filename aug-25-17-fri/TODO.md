6317240af0e5571aaa74a2325dace5dd50610ddd
Jorge Andrade <jorandradefig@gmail.com>
2017-08-25 11:37:58 -0500

    add: TODO.md

---
+++ aug-25-17-fri/TODO.md
@@ -0,0 +1,5 @@
+# Actividad
+
+1. Crear una App en Giphy
+2. Crear un catálogo de Gifs
+3. Permitir un input para buscar los gifs dinámicamente
--- aug-25-17-fri/jorge/src/app/app.component.ts
+++ aug-25-17-fri/jorge/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  gifs: object[];
+  q: string;
+
+  constructor(private http: Http) {}
+
+  onKey(event: any) {
+    this.q = event.target.value;
+
+    this.http
+    .get('https://api.giphy.com/v1/gifs/search',
+      {
+        params: {
+          api_key: 'fe81ce12f2484d82bbd9084f16c18ef7',
+          q: this.q,
+          limit: 10,
+          rating: 'G',
+          lang: 'en'
+        }
+      }
+    )
+    .subscribe((res: Response) => this.gifs = res.json());
+  }
+
+  ngOnInit() {
+
+  }
 }
