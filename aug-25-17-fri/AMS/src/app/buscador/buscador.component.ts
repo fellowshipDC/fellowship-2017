d617ce6e2ca230ddfc1039289c353972ed5422cd
unknown <mirandas.angelica@gmail.com>
2017-08-28 14:29:23 -0500

    componentes input home y titulo

--- aug-25-17-fri/AMS/src/app/buscador/buscador.component.ts
+++ aug-25-17-fri/AMS/src/app/buscador/buscador.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-buscador',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscadorComponent implements OnInit {
 
-  constructor() { }
+  q: string;
+  gifs:object;
+
+  constructor(private http: Http) { }
+
+  onKey(event: any){
+    this.q = event.target.value;
+
+    this.http.get('https://api.giphy.com/v1/gifs/search',{
+      params: {
+        'api_key': '73ad934950184ba0afc88a67a77095d1',
+        q: this.q,
+        limit: 9,
+        offset: 0,
+        rating: 'G',
+        lang: 'en'
+      }
+    }).subscribe((res: Response) => this.gifs = res.json());
+  }
 
   ngOnInit() {
+    this.http.get('https://api.giphy.com/v1/gifs/search',{
+      params: {
+        'api_key': '73ad934950184ba0afc88a67a77095d1',
+        q: 'hello',
+        limit: 3,
+        offset: 5,
+        rating: 'G',
+        lang: 'en'
+      }
+    }).subscribe((res: Response) => this.gifs = res.json());
   }
 
 }
--- aug-25-17-fri/AMS/src/app/home/home.component.ts
+++ aug-25-17-fri/AMS/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 @Component({
@@ -6,39 +6,8 @@ import { Http, Response } from '@angular/http';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.styl']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  gifs: object;
-  q: string;
-
-  constructor(private http: Http) { }
-
-  onKey(event: any){
-    this.q = event.target.value;
-
-    this.http.get('https://api.giphy.com/v1/gifs/search',{
-      params: {
-        'api_key': '73ad934950184ba0afc88a67a77095d1',
-        q: this.q,
-        limit: 12,
-        offset: 0,
-        rating: 'G',
-        lang: 'en'
-      }
-    }).subscribe((res: Response) => this.gifs = res.json());
-  }
-
-  ngOnInit() {
-    this.http.get('https://api.giphy.com/v1/gifs/search',{
-      params: {
-        'api_key': '73ad934950184ba0afc88a67a77095d1',
-        q: this.q,
-        limit: 12,
-        offset: 0,
-        rating: 'G',
-        lang: 'en'
-      }
-    }).subscribe((res: Response) => this.gifs = res.json());
-  }
+  constructor() { }
 
 }
