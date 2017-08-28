1a0a586eb968867b8d568fe394d24c7b3957e9d8
nellalela <nella.rg25@gmail.com>
2017-08-28 15:05:55 -0500

    ya casi

---
+++ aug-28-17-mon/Nelly/Ejercicio Jorge/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [],
+    component: HomeComponent
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
