a1ef89fe43996ca64fbda2e05a5bac11b5f45fac
unknown <mirandas.angelica@gmail.com>
2017-08-29 14:50:19 -0500

    ejercicio petco

---
+++ aug-29-17-tue/AMS/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { DogComponent } from './dog/dog.component';
+import { ImagenComponent } from './imagen/imagen.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DogComponent,
+    ImagenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
