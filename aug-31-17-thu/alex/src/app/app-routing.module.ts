f377f9e9b4f1338d437c5542f485d8b5c17fa60b
alexfuser <alexfuser@mozilla-mexico.org>
2017-09-03 23:50:14 -0500

    Proyecto Breed vista galleria

--- aug-31-17-thu/alex/src/app/app-routing.module.ts
+++ aug-31-17-thu/alex/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DataComponent } from './data/data.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    children: [],
+    component: HomeComponent
+  },
+  {
+    path: 'data',
+    children: [],
+    component: DataComponent
   }
 ];
 
