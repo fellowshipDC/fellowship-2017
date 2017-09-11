5b36f08f12ca002b334403eba1a9eb89ae4eaecf
unknown <mirandas.angelica@gmail.com>
2017-09-11 09:46:34 -0500

    sails features

--- sep-7-17-thu/AMS/src/app/app.module.ts
+++ sep-7-17-thu/AMS/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CustomComponent } from './custom/custom.component';
+import { FeaturesComponent } from './features/features.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    CustomComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
