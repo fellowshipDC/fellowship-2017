881e36bbf96e797986df73498b5087730d668f2a
Paola <Paola@MacBook-Pro-de-Paola.local>
2017-09-15 00:57:26 -0500

    add: vuejs

---
+++ sep-5-17-tue/paola/src/app/patreon/patreon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patreon',
+  templateUrl: './patreon.component.html',
+  styleUrls: ['./patreon.component.css']
+})
+export class PatreonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
