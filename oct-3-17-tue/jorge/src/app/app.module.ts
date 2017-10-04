3971bfbb22044a11cee5cec67373355639d0e824
nellalela <nella.rg25@gmail.com>
2017-10-04 09:34:05 -0500

    componente circulos

--- oct-3-17-tue/jorge/src/app/app.module.ts
+++ oct-3-17-tue/jorge/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NellyComponent } from './nelly/nelly.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NellyComponent
   ],
   imports: [
     BrowserModule,
