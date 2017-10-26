03994bb47e44eb10c3520d39fcc4489db3bdc329
Jose Luis <puenterodriguezjose@gmail.com>
2017-10-26 08:56:14 -0500

    Movies Scatter graph

--- oct-24-17-tue/jorge/src/app/app.module.ts
+++ oct-24-17-tue/jorge/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScatterComponent } from './scatter/scatter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScatterComponent
   ],
   imports: [
     BrowserModule,
